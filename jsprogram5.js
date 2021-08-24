db0b86ff2291401f23db503634b75a13b387c289
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-24 21:20:52 +0530

    swap 2 variables
    javascript program to swap the values of two variables.

---
+++ jsprogram5.js
@@ -0,0 +1,13 @@
+//javascript program to swap two variables.
+let num1 = 5;
+let num2 = 4;
+
+console.log(num1);
+console.log(num2);
+
+num1 = num1 + num2;
+num2 = num1 - num2;
+num1 = num1 - num2;
+
+console.log(num1);
+console.log(num2);
