6bebe3c00bb97c98a74be4b6dc31f0b7859bc011
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-25 20:46:39 +0530

    check starting and ending character
    javascript program to check with what character the string starts with and ends with.

---
+++ jsprogram33.js
@@ -0,0 +1,5 @@
+//javascript program to check whether a string starts with or ends with a certain character.
+//first method
+let string = "peter parker, here to pick up a passport please";
+console.log(string + " : starts with the character : " + string[0] + " || " + string + " : ends with the character : " + string[string.length-1]);
+
