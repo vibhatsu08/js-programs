be86d5dc688add4e7097612f17046b31fc73cd2e
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-25 17:39:06 +0530

    first letter uppercase
    javascript program to convert the first letter of a string to uppercase.

---
+++ jsprogram30.js
@@ -0,0 +1,3 @@
+//javascript program to convert the first letter of a string to uppercase
+let string = "peter parker here to pick up a passport please.";
+console.log(string.replace(string[0], string[0].toUpperCase()));
