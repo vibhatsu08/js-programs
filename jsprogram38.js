94b24d0f75bb56be5aa9c46b3ce4cfbb18f6c2c9
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-25 21:31:30 +0530

    trim string
    javascript program to trim a string using the trim() method, removing the extra whitespaces.

---
+++ jsprogram38.js
@@ -0,0 +1,4 @@
+//javascript program to trim a strin , i.e., remove the extra whitespaces from both ends of a given string.
+let string = "                            hello world                             ";
+console.log(string);
+console.log(string.trim());
