805091fca6b9b1773c5ff4a0211d4ad74f865fd9
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-25 17:05:53 +0530

    sort alphabets
    javascript program to sort a given string alphabetically

---
+++ jsprogram26.js
@@ -0,0 +1,7 @@
+//javascript program to sort the letters of a word alphabetically
+let string = "spiderman no way home";
+console.log(string);
+let stringSplit = string.split('');
+let stringSort = stringSplit.sort();
+let stringSorted = stringSort.join('');
+console.log(stringSorted);
