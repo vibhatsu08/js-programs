3dac6b4609d2295fa1900ae816c98ddc9f42c743
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-25 17:09:44 +0530

    replace characters
    javascript program to replace characters in a given string.

---
+++ jsprogram27.js
@@ -0,0 +1,6 @@
+//javascript program to replace characters in a given string.
+let string = "spiderman no way home";
+console.log(string);
+const change = /e/g;
+let replaceString = string.replace(change, 'z');
+console.log(replaceString);
