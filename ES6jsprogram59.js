fa0c6c9d0d50312bd7d8d8cbaf759f2615ad30ef
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 17:13:18 +0530

    Delete ES6jsprogram59.js

--- ES6jsprogram59.js
+++
@@ -1,8 +0,0 @@
-//ES6 program to define a function in a program.
-//older Javascript syntax to define a function.
-let x = function addNumbers(x, y){
-    return x + y;
-}
-
-//newer ES6 syntax to define functions using arrow functions.
-let x = (x, y) => x + y;
