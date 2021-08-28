236c3117af5361ad52fa040a8ff4357500ce9e4b
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 17:00:18 +0530

    ES^ const keyword
    By using the const keyword on a variable, the value cannot be changed later in the program. Trying to change the value of the const variable, would result in an error.

---
+++ ES6jsprogram58.js
@@ -0,0 +1,6 @@
+//ES6 js program to declare a variable using the 'const' keyword. Declare a 'const', prevents from changing the value of the variable in the program.
+const num = 20;
+console.log(num); //value is 20.
+num = 10;
+console.log(num); //error, because a const value cannot be changed.
+ 
