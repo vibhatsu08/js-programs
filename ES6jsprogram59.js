1fc91c0908f4bc092276b26569c11265270defdb
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 17:11:49 +0530

    ES6 program functions
    Older javascript functions in JS, newer method includes Javascript arrow functions.

---
+++ ES6jsprogram59.js
@@ -0,0 +1,8 @@
+//ES6 program to define a function in a program.
+//older Javascript syntax to define a function.
+let x = function addNumbers(x, y){
+    return x + y;
+}
+
+//newer ES6 syntax to define functions using arrow functions.
+let x = (x, y) => x + y;
