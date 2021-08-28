9464f6e795cb1f29804626d80c7bda68a951eda1
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 17:12:52 +0530

    Delete ES6jsprogram58.js

--- ES6jsprogram58.js
+++
@@ -1,6 +0,0 @@
-//ES6 js program to declare a variable using the 'const' keyword. Declare a 'const', prevents from changing the value of the variable in the program.
-const num = 20;
-console.log(num); //value is 20.
-num = 10;
-console.log(num); //error, because a const value cannot be changed.
- 
