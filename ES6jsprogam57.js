b4e5afb93b84a9b7bbbe3e6dd88c02f4118c12a8
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-28 16:54:53 +0530

    ES6 program to declare variable.
    ES6 program to declare variables using the let keyword, since the 'let' keyword is block scoped.

---
+++ ES6jsprogam57.js
@@ -0,0 +1,9 @@
+//ES6 program to declare variables.
+let num = 10;
+{
+    let num = 20;
+    console.log(num);
+}
+console.log(num);
+
+//the 'let' keyword is used to declare varibles in the newer JS. The let keyword is a block scope variable. The value of a 'let' defined variable can change on redeclaration.
